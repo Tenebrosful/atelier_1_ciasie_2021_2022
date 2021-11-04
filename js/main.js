719ef51f612feae783e6d3cb48e7466704c21a0f
bernar323u <hugobernard39@gmail.com>
2021-11-04 10:38:40 +0100

    Use input.max and input.min

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function plus(input){
-    input.value = parseInt(input.value) + 1
+    input.value = Math.min(parseInt(input.value) + 1, input.max || Number.MAX_VALUE)
 }
 
 function minus(input){
-    input.value = Math.max(parseInt(input.value) - 1, 0)
+    input.value = Math.max(parseInt(input.value) - 1, input.min || -Number.MAX_VALUE)
 }
 
 but1 = document.getElementById("button_prod");
