3aaed3e46b7f2e4c6c6f8ad2a1ad642871165020
BLOT <tristan.blot3@etu.univ-lorraine.fr>
2021-11-04 11:26:38 +0100

    mise a jour script

--- script.sql
+++ script.sql
@@ -37,7 +37,7 @@ CREATE TABLE `product` (
   `url_img` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE `order` (
+CREATE TABLE `orderr` (
   `id` int(11) NOT NULL,
   `client_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `client_adress` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -47,12 +47,14 @@ CREATE TABLE `order` (
   `delivred` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE `productOrder` (
-  `order_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `amount` int(11) NOT NULL,
-  `total_price` decimal(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE productorder 
+(product_id INT NOT NULL, 
+order_id INT NOT NULL, 
+quantity INT NOT NULL, 
+INDEX IDX_25E4C1224584665A (product_id), 
+INDEX IDX_25E4C1228D9F6D38 (order_id), 
+PRIMARY KEY(product_id, order_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB;
+
 
 CREATE TABLE `producer` (
   `id` int(11) NOT NULL,
@@ -63,22 +65,17 @@ CREATE TABLE `producer` (
   `url_img` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB;
 
 
 INSERT INTO `product` (`id`, `name`, `price`, `amount_unit`, `unit`, `description`, `url_img`) VALUES
 (1, 'Lait BIO', '0.80', '0.80', '1L', 'Lait BIO produit dans une ferme par des chèvre unijambiste', 'https://catalog-media.lafourche.fr/naturavenir-lait-de-vache-entier-sterilise-france-bio-075litre-582b89986c4a51bcce48d0e231168537d59b67c4.png?width=1080&quality=75'),
 (2, 'Crème fraiche BIO', '7.20', '1.80', '20cl', 'Crème fraiche mais trop', 'https://panieronaturel.com/wp-content/uploads/2020/01/cr%C3%A8me-fraiche.jpg'),
 (3, 'Navet BIO', '2', '2', '1kg', 'Navet', 'https://www.meillandrichardier.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/4/4/4461-4462-navet_de_nancy-ho-tg171102.jpg');
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQ_F06D39705E237E06` (`name`);
-  
-ALTER TABLE `productOrder`
-  ADD PRIMARY KEY (`order_id`,`product_id`);
+-
 
 
-INSERT INTO `order` (`id`, `client_name`, `client_adress`, `client_email`, `client_phone`, `total_price`,`delivred` ) VALUES
+INSERT INTO `orderr` (`id`, `client_name`, `client_adress`, `client_email`, `client_phone`, `total_price`,`delivred` ) VALUES
 (1, 'BLOT', '8 rue des Rouliers', 'tristan.blot@7.com', '0674954544', '25', 1),
 (1, 'DUPONT', '26 boulevard des Colombes', 'dupoint@orange.com', '0774951544', '12', 0),
 (1, 'TULIPE', '12 AVENUE DES TRUITES', 'bellefleur@hotmail.fr', '0774951544', '19', 0);
@@ -90,8 +87,7 @@ INSERT INTO `producer` (`id`, `name`, `adress`, `email`, `phone`, `url_img`) VAL
 (3, 'M.petit', '2 rue du chateau', 'e9txjt64s8b@temporary-mail.net', '0698764356', 'https://cocktail-graphic.com/wp-content/uploads/2017/11/Logo-FERME-AULNAYS.jpg');
 
 
-INSERT INTO `productOrder` (`order_id`, `product_id`, `amount`, `total_price`) VALUES
-(1, 1, 4, 4),
-(2, 1, 4, 3),
-(1, 2, 3, 5);
-
+INSERT INTO productorder (order_id, product_id, quantity) VALUES
+(1, 1, 4),
+(2, 1, 4),
+(1, 2, 3);
