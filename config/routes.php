8beafa2931ead9fcee478413b12be97d6b573d72
Geoffrey <geoffrey.poraykostexp7@gmail.com>
2021-11-04 18:05:51 +0100

    categories

--- config/routes.php
+++ config/routes.php
@@ -6,25 +6,25 @@ use Doctrine\ORM\EntityManager;
 use Slim\Views\Twig;
 
 require_once '../src/Controllers/ProductController.php';
+require_once '../src/Controllers/CategoryController.php';
 require_once '../src/Controllers/OrderController.php';
 
 $app->get('/',function ($request, $response, array $args){
     $pc = new ProductController($this->get(EntityManager::class));
+    $ct = new CategoryController($this->get(EntityManager::class));
     $products = $pc->getAll();
-    return $this->get(Twig::class)->render($response,"index.html.twig", ['products' => $products]);
+    $categories = $ct->getAll();
+    return $this->get(Twig::class)->render($response,"index.html.twig", ['products' => $products, 'categories' => $categories]);
 });
 
 $app->get('/{id}', function ($request, $response, array $args){
     $pc = new ProductController($this->get(EntityManager::class));
     $prod = $pc->getById($args['id']);
     return $this->get(Twig::class)->render($response,"detail.html.twig", ['prod' => $prod]);
-}); 
+});
 
 $app->get('/coop/',function ($request, $response, array $args){
     $pc = new OrderController($this->get(EntityManager::class));
     $order = $pc->getAll();
     return $this->get(Twig::class)->render($response,"cooperative.html.twig", ['order' => $order]);
 });
-
-
-
