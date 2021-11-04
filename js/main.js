a25ff729f2f2b23c7829989ecb29fd0326424962
Bastien Hohler <bastien.hohler@telecomnancy.eu>
2021-11-04 08:45:23 +0100

    Modifs page signIn

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-function plus(input){
+function plus(input) {
     input.value = parseInt(input.value) + 1
 }
 
-function minus(input){
+function minus(input) {
     input.value = Math.max(parseInt(input.value) - 1, 0)
 }
 
@@ -11,30 +11,38 @@ but2 = document.getElementById("button_gerant");
 but1.clicked = false;
 but2.clicked = false;
 
-but1.onclick = function() {
-   this.clicked = !this.clicked;
-   let target = document.getElementById('hidden1');
+but1.onclick = function () {
+    this.clicked = !this.clicked;
+    let target = document.getElementById('hidden1');
     if (this.clicked) {
         if (but2.clicked) {
             but2.click();
         }
-        target.style.height = target.scrollHeight+"px";
+        this.style.backgroundColor = '#f42400';
+        this.style.color = '#e5e5e5';
+        target.style.height = target.scrollHeight + "px";
     }
     else {
+        this.style.backgroundColor = '#ff4b2b';
+        this.style.color = 'white';
         target.style.height = 0;
     }
 }
 
-but2.onclick = function() {
+but2.onclick = function () {
     this.clicked = !this.clicked;
     let target = document.getElementById('hidden2');
-     if (this.clicked) {
+    if (this.clicked) {
         if (but1.clicked) {
             but1.click();
         }
-         target.style.height = target.scrollHeight+"px";
-     }
-     else {
-         target.style.height = 0;
-     }
- }
+        this.style.backgroundColor = '#f42400';
+        this.style.color = '#e5e5e5';
+        target.style.height = target.scrollHeight + "px";
+    }
+    else {
+        this.style.backgroundColor = '#ff4b2b';
+        this.style.color = 'white';
+        target.style.height = 0;
+    }
+}
