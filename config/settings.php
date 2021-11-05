1cfeea349eb31f454647cc3f5e5307205fcea2b3
bernar323u <hugobernard39@gmail.com>
2021-11-05 10:39:46 +0100

    Switch to env var for database logging

--- config/settings.php
+++ config/settings.php
@@ -2,44 +2,42 @@
 
 // settings.php
 use DI\ContainerBuilder;
+
 define('APP_ROOT', __DIR__);
 
 return function (ContainerBuilder $containerBuilder) {
-    $containerBuilder->addDefinitions([
-    'settings' => [
-        'displayErrorDetails' => true,
-        'determineRouteBeforeAppMiddleware' => false,
+    if (is_array($bddConfig = parse_ini_file(__DIR__ . '/config/bdd.ini')))
+        $containerBuilder->addDefinitions([
+            'settings' => [
+                'displayErrorDetails' => true,
+                'determineRouteBeforeAppMiddleware' => false,
 
-        'doctrine' => [
-            // if true, metadata caching is forcefully disabled
-            'dev_mode' => true,
+                'doctrine' => [
+                    // if true, metadata caching is forcefully disabled
+                    'dev_mode' => true,
 
-            // path where the compiled metadata info will be cached
-            // make sure the path exists and it is writable
-            'cache_dir' => APP_ROOT . '/var/doctrine',
+                    // path where the compiled metadata info will be cached
+                    // make sure the path exists and it is writable
+                    'cache_dir' => APP_ROOT . '/var/doctrine',
 
-            // you should add any other path containing annotated entity classes
-            'metadata_dirs' => [APP_ROOT . '/../src/database/models'],
+                    // you should add any other path containing annotated entity classes
+                    'metadata_dirs' => [APP_ROOT . '/../src/database/models'],
 
-            'connection' => [
-                'driver' => 'pdo_mysql',
-                'host' => 'localhost',
-                'port' => 3306,
-                'dbname' => 'lehangarlocal',
-                'user' => 'blot32u',
-                'password' => 'blot32u',
-                'charset' => 'utf8'
-            ]
-            ],
-            'twig' => [
-                'paths' => [
-                    __DIR__ .'/../src/views'
+                    'connection' => $bddConfig
+                ],
+                'twig' => [
+                    'paths' => [
+                        __DIR__ . '/../src/views'
                     ],
-                'options' => [
-                    'cache_enabled' => false,
-                    'cache_path' => __DIR__ . '/../var/twig',
+                    'options' => [
+                        'cache_enabled' => false,
+                        'cache_path' => __DIR__ . '/../var/twig',
                     ]
                 ]
-    ]
-]);
+            ]
+        ]);
+    else {
+        echo "ERREUR : Fichier de configuration de la base de donnée introuvable";
+        exit();
+    }
 };
