c63a176a418efa5aa736494bf45e545f51645b82
bernar323u <hugobernard39@gmail.com>
2021-11-05 19:10:35 +0100

    Fix events

--- public/js/coop.js
+++ public/js/coop.js
@@ -1,10 +1,9 @@
 const checkbox = document.querySelectorAll("input");
 var completed = document.getElementById("completed-tasks");
 var incompleted = document.getElementById("incomplete-tasks");
-const boutons = document.querySelectorAll("button");
+const boutonsDesc = document.querySelectorAll("button");
 const btnclose = document.querySelectorAll(".btnClose")
 var description = document.querySelectorAll("div");
-
 
 checkbox.forEach(e => {
   e.addEventListener('change', function() {
@@ -26,22 +25,22 @@ btnclose.forEach(btnc =>{
   btnc.addEventListener('click',closeModal);
 })
 
-boutons.forEach(btn => {
-  btn.addEventListener('click',openModal);
+boutonsDesc.forEach(btnDesc => {
+  btnDesc.addEventListener('click',openModal);
 })
 
 description.forEach(pop =>{
   description.innerHTML = pop.parentNode.className;
 })
 
-function closeModal() {
-  var id = btnc.parentNode.parentNode.id;
+function closeModal(e) {
+  var id = e.currentTarget.parentNode.parentNode.id;
   const popup = document.getElementById(id+"des");
   popup.style.display='none';
 }
 
-function openModal() {
-  var id = btn.parentNode.id;
+function openModal(e) {
+  var id = e.currentTarget.parentNode.id;
   const popup = document.getElementById(id+"des");
   popup.style.display = "block";
 }
