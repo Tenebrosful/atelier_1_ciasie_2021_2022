26b21478e668d1dbb1601594a82eda76f3ad35d2
bernar323u <hugobernard39@gmail.com>
2021-11-06 10:25:34 +0100

    Fix redirect

--- config/routes.php
+++ config/routes.php
@@ -121,7 +121,7 @@ $app->post('/signIn', function ($request, $response) {
     if(isset($_SESSION["messageErrorSignin"]) &&  (!isset($_SESSION["userId"]))){
         return $this->get(Twig::class)->render($response, 'signIn.html.twig', ['messageError' => $_SESSION["messageErrorSignin"]]);
     } else {
-        header("Location:http://localhost:8080/");
+        header("Location:/");
         exit();
     }
 });
